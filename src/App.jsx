a6c44ab108711b203d512e7187f7b79c67d8b2a8
Vidhi1010 <vidhisinghvp1010@gmail.com>
2025-10-26 23:40:01 +0530

    implement left-empty layout as per assignment requirements

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,13 @@ import GalleryWidget from "./components/GalleryWidget";
 
 function App() {
   return (
-    <div className="min-h-screen bg-[#191B1F] flex justify-center items-center p-8 text-white">
-      <div className="flex flex-col lg:flex-row gap-8">
-        <div className="flex flex-col">
+     <div className="min-h-screen bg-[#191B1F] flex text-white">
+      {/* Left half - Empty */}
+      <div className="w-1/2 hidden lg:block"></div>
+      
+      {/* Right half - Components */}
+      <div className="w-full lg:w-1/2 flex justify-center items-center p-8">
+        <div className="flex flex-col gap-8">
           <Tabs />
           <GalleryWidget />
         </div>
